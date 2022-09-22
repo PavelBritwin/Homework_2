7d02d322d484923a0a772aae65eef1c46dd28982
Pavel Anokhin <anohinpa@gmail.com>
2022-09-22 20:40:41 +0300

    Изменен комментарий ввода числа

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Task10
 Console.Clear();
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Вторая цифра числа: ");
 Console.WriteLine((number / 10) % 10);
