d8113f3e95b7f3b20b7d572f04818a76d9d811ed
Pavel Anokhin <anohinpa@gmail.com>
2022-09-23 21:22:05 +0300

    Task 13

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
 Console.Clear();
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+string numberStr = number.ToString();
 
-if (number % 100 < 1) Console.WriteLine("Третьей цифры нет");
+if (number < 100) Console.WriteLine("Третьей цифры нет");
 else 
 {
     Console.WriteLine("Третья цифра числа:");
-    Console.WriteLine((number % 10) % 100);
+    Console.WriteLine(numberStr[2]);
 }
